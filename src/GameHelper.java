5c5abb8334b5ac7ad6b4efe2d7aac1651954f810
xMonitoring\x_Monitoring <demciuc.serghei@gmail.com>
2021-04-09 16:27:33 +0300

    Дополнено 2 класса

---
+++ src/GameHelper.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class GameHelper {
+
+    private static final String alphabet = "abcdefg";
+    private int gridLength = 7;
+    private int gridSize = 49;
+    private int [] grid = new int[gridSize];
+    private int comCount = 0;
+
+    public String getUserInput (String promt) {
+        String inputLine = null;
+        System.out.println(promt + " ");
+        try {
+            BufferedReader is = new BufferedReader(
+                    new InputStreamReader(System.in);
+                    inputLine = is.readLine();
+                    if (inputLine.length() == 0) return null;
+            ) catch (IOException e){
+            System.out.println("\"IOException: " + e");
+        }
+        return inputLine.toLowerCase();
+    }
+        public ArrayList<String> placeDotCom(int comSize) {
+            ArrayList<String> alphaCells = new ArrayList<String>();
+            
+        }
+}
